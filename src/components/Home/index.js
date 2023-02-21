739a3f0f88c23aa46db73c3642501af820331542
Kishore <kishorebalueeda4@gmail.com>
2023-02-21 09:46:38 +0000

    bookHub

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -14,14 +14,14 @@ const settings = {
   dots: false,
   infinite: false,
   speed: 500,
-  slidesToShow: 3,
+  slidesToShow: 4,
   slidesToScroll: 1,
   initialSlide: 0,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 2,
+        slidesToShow: 3,
         slidesToScroll: 1,
         infinite: true,
         dots: false,
@@ -38,7 +38,7 @@ const settings = {
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1,
       },
     },
@@ -185,10 +185,10 @@ class Home extends Component {
 
   render() {
     return (
-      <>
+      <div className="main-home-container">
         <Header />
         <div className="home-container1">{this.renderHomeContainer()}</div>
-      </>
+      </div>
     )
   }
 }
